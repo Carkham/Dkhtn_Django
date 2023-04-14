861254908a159a7ed70c9d7e7e320e0eec0b3a95
Carkham <13193102258@163.com>
2023-04-14 14:55:34 +0800

    [chore]: 新增flake8

--- dkhtn_django/conftest.py
+++ dkhtn_django/conftest.py
@@ -1,14 +1,6 @@
 import pytest
 
-# from dkhtn_django.users.models import User
-# from dkhtn_django.users.tests.factories import UserFactory
 
-
-# @pytest.fixture(autouse=True)
-# def media_storage(settings, tmpdir):
-#     settings.MEDIA_ROOT = tmpdir.strpath
-#
-#
-# @pytest.fixture
-# def user(db) -> User:
-#     return UserFactory()
+@pytest.fixture()
+def hello_world():
+    return "Hello World!"
