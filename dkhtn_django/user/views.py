8ed3589bbf35e1496b1772b17b6dbf7fdaaae15f
Xbuluo <92033244+Xbuluo@users.noreply.github.com>
2023-04-16 20:41:12 +0800

    [Feature]:添加user的email验证 (#12)
    * [feature]:邮件验证
    
    * [feature]:python stmplib库完成邮件验证
    
    * [feature]:rabbitmq_host放入settings/base.py
    
    * [feature]:添加邮件html样式
    
    * [feature]:单元测试
    
    * [feature]:添加setting.test.py的DEBUG
    
    * [fix]:删除test.py的空行
    
    * [chore]: delete __init__
    
    * [fix]: Update dkhtn_django/user/rabbit/EmailSender.py
    
    Co-authored-by: Carkham <60054018+Carkham@users.noreply.github.com>
    
    * [fix]: Update dkhtn_django/user/views.py
    
    Co-authored-by: Carkham <60054018+Carkham@users.noreply.github.com>
    
    * [fix]: format
    
    ---------
    
    Co-authored-by: Carkham <60054018+Carkham@users.noreply.github.com>
    Co-authored-by: Carkham <13193102258@163.com>

--- dkhtn_django/user/views.py
+++ dkhtn_django/user/views.py
@@ -1,3 +1,23 @@
 # from django.contrib.auth.models import User
+from django.http import JsonResponse
+from .rabbit.RabbitMQ import RabbitMQ
+from .rabbit.EmailSender import send_email
+from django.conf import settings
 
-# Create your views here.
+
+def email_send(request):
+    email = request.GET.get('email')
+    # FIXME: 我靠要是没有'email'字段怎么办
+    if settings.DEBUG:
+        send_email(email)
+    else:
+        RabbitMQ(email)
+    ret = {
+        "code": 0,
+        "message": "验证码发送成功",
+    }
+    return JsonResponse(ret)
+
+
+def email_check(request):
+    pass
