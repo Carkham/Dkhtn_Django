a3d3b3d1ae29bc0f7ada7634ad76e2039a0562b8
Xbuluo <92033244+Xbuluo@users.noreply.github.com>
2023-04-16 20:24:46 +0800

    Update dkhtn_django/user/views.py
    Co-authored-by: Carkham <60054018+Carkham@users.noreply.github.com>

--- dkhtn_django/user/views.py
+++ dkhtn_django/user/views.py
@@ -7,6 +7,7 @@ from django.conf import settings
 
 def email_send(request):
     email = request.GET.get('email')
+    #FIXME: 我靠要是没有'email'字段怎么办
     if settings.DEBUG:
         send_email(email)
     else:
