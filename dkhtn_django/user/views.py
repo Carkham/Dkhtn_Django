6c98697ef13341e25a0d76d88b95cfa97e6e949f
Maryin-c <3317701822@qq.com>
2023-04-19 12:40:51 +0800

    [feature]:增加注册接口

--- dkhtn_django/user/views.py
+++ dkhtn_django/user/views.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 
 from ..utils.json_req_parser import JsonReq
 from .rabbit.RabbitMQ import rabbit_mq
-from .wrappers import wrapper_set_login, wrapper_verify_send, wrapper_verify_check
+from .wrappers import wrapper_set_login, wrapper_verify_send, wrapper_verify_check, wrapper_register
 from .models import User
 
 
@@ -90,3 +90,55 @@ def login(request):
         #     "message": e.__str__(),
         # }
         # return JsonResponse(response)
+
+
+@wrapper_register
+def register(request):
+    """
+    注册，流程为->（前端）先调用获取邮箱验证码接口->（后端）发送验证邮件，建立session id，存储验证码，反馈前端
+    ->（前端）调用用户注册接口->（后端）检验session id，检验验证码，注册成功则直接登录
+    :param request:
+    :return:
+    """
+    try:
+        _request = JsonReq(request)
+        username = _request.POST.get('uname')
+        password = _request.POST.get('password')
+        email = _request.POST.get('email')
+        avatar = _request.POST.get('avatar')
+        # 检查重名
+        old_user = User.objects.filter(username=username)
+        if old_user:
+            response = {
+                "code": 2,
+                "message": "用户名已存在",
+            }
+            return JsonResponse(response)
+        # 检查邮箱重复
+        old_user = User.objects.filter(email=email)
+        if old_user:
+            response = {
+                "code": 3,
+                "message": "邮箱已存在",
+            }
+            return JsonResponse(response)
+        # 进行注册
+        user = User.objects.create_user(username=username,
+                                        password=password,
+                                        avatar=avatar,
+                                        email=email)
+        # 登录信息登记
+        request.userinfo = user.userinfo()
+        response = {
+            "code": 0,
+            "message": "注册成功",
+        }
+        return JsonResponse(response)
+    except Exception as e:
+        raise e
+        # response = {
+        #     "code": 114514,
+        #     "message": e.__str__(),
+        # }
+        # return JsonResponse(response)
+
