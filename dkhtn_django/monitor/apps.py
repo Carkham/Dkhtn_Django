2a393d9413233189413248beb594ddb3372ca0d1
buluo1204 <2076989467@qq.com>
2023-04-20 16:11:33 +0800

    [fix](monitor.py):修改name

--- dkhtn_django/monitor/apps.py
+++ dkhtn_django/monitor/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class MonitorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'monitor'
+    name = 'dkhtn_django.monitor'
 
     def ready(self):
         from .resource import MonitorConsuming
