35ebc2330351755bd9c1ce9103f301917d1512a2
wit <19376471@buaa.edu.cn>
2023-04-23 22:41:54 +0800

    [feature]:监控模块修改资源数据库、增加单元测试

--- dkhtn_django/monitor/apps.py
+++ dkhtn_django/monitor/apps.py
@@ -4,8 +4,3 @@ from django.apps import AppConfig
 class MonitorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dkhtn_django.monitor'
-
-    def ready(self):
-        from .resource import MonitorConsuming
-        monitor = MonitorConsuming()
-        monitor.start()
--- dkhtn_django/monitor/tests.py
+++ dkhtn_django/monitor/tests.py
@@ -1,1 +1,24 @@
 # Create your tests here.
+import pytest
+from django.test import Client
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "urls, expected",
+    [
+        (
+            "/api/resource/query-resource",
+            {
+                "code": 0,
+                "msg": "",
+                "data": [
+                ]
+            }
+        )
+    ]
+)
+def test_query_resource(urls, expected):
+    client = Client()
+    response = client.get(urls)
+    assert expected == response.json()
