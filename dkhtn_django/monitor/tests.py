59d493ba1f4c916f1febd7e075b0a083edabe360
wit <19376471@buaa.edu.cn>
2023-04-23 22:10:58 +0800

    [feature]:监控模块修改测试样例

--- dkhtn_django/monitor/tests.py
+++ dkhtn_django/monitor/tests.py
@@ -13,16 +13,6 @@ from django.test import Client
                 "code": 0,
                 "msg": "",
                 "data": [
-                    {
-                        "func-id": 114514,
-                        "state": 1,
-                        "cpu": 0,
-                        "memory": 0,
-                        "run_time": 0,
-                        "times": 0,
-                        "error": 0,
-                        "cost": 0
-                    }
                 ]
             }
         )
